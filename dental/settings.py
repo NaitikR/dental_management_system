a50a2911fd4861123e8fc9a53dd6a4ca1a572ef7
Naitik Rathod <naitikrathod@Naitiks-MacBook-Air.local>
2024-09-22 19:49:36 -0400

    second commit

--- dental/settings.py
+++ dental/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aq&!084u==1xqf4(g8_ep!f#fnt98tu=ie*xr=he9t+m5rw*9z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=True
